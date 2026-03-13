394b1db99a71cd3d125214660e5d76e32c3a8b88
Naman Manjkhola <namanmanjkhola@gmail.com>
2026-03-13 14:52:02 +0530

    Restructured Project and added embedding service in python

---
+++ backend/src/main/java/com/gyan/ai/AIService.java
@@ -0,0 +1,7 @@
+package com.gyan.ai;
+
+import java.util.List;
+
+public interface AIService {
+    List<Double> generateEmbedding(String text);
+}
---
+++ backend/src/main/java/com/gyan/service/EmbeddingService.java
@@ -0,0 +1,7 @@
+package com.gyan.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmbeddingService {
+    
+}
