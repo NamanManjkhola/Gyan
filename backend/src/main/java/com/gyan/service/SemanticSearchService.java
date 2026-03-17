ab6712d3015c9a4a39d75e21ebfade3fce7c6ea5
Naman Manjkhola <namanmanjkhola@gmail.com>
2026-03-17 23:32:52 +0530

    Added question Answers service using Groq ai api

--- backend/src/main/java/com/gyan/service/SemanticSearchService.java
+++ backend/src/main/java/com/gyan/service/SemanticSearchService.java
@@ -55,4 +55,53 @@ public class SemanticSearchService {
 
         return result;
     }
+
+    public List<DocumentChunk> findRelevantChunks(String query) throws Exception {
+
+        List<Double> queryEmbedding =
+                embeddingService.generateEmbedding(query);
+
+        List<DocumentChunk> chunks =
+                documentChunkRepository.findAll();
+
+        List<Map.Entry<DocumentChunk, Double>> scores =
+                calculateScores(queryEmbedding, chunks);
+
+        return scores.stream()
+                .sorted((a, b) ->
+                        Double.compare(b.getValue(), a.getValue()))
+                .limit(5)
+                .map(Map.Entry::getKey)
+                .toList();
+    }
+
+    private List<Map.Entry<DocumentChunk, Double>> calculateScores(
+            List<Double> queryEmbedding,
+            List<DocumentChunk> chunks) throws Exception {
+
+        List<Map.Entry<DocumentChunk, Double>> scores = new ArrayList<>();
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        for (DocumentChunk chunk : chunks) {
+
+            if (chunk.getEmbeddingVector() == null) continue;
+
+            List<Double> chunkEmbedding =
+                    mapper.readValue(
+                            chunk.getEmbeddingVector(),
+                            List.class
+                    );
+
+            double similarity =
+                    VectorSimilarityUtil.cosineSimilarity(
+                            queryEmbedding,
+                            chunkEmbedding
+                    );
+
+            scores.add(Map.entry(chunk, similarity));
+        }
+
+        return scores;
+    }
 }
